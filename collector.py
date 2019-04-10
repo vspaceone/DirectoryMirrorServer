455f4b24a08ff3eb3d7fb5a0cbf0d642bf81bb20
noppelmax <max@noppelmax.online>
2019-04-10 18:28:21 +0200

    new collector without true false values

--- collector.py
+++ collector.py
@@ -10,7 +10,7 @@ from influxdb import InfluxDBClient
 from multiprocessing import Process, Manager
 
 DBNAME = "SPACEAPI_STATS"
-USE_INFLUX = False
+USE_INFLUX = True
 USE_EXPORT_DIRECTORY = True
 
 
@@ -55,15 +55,18 @@ def loadSpaceAPI(spacename,url,points,directory):
                     logger.info("invalid")
 
                 directory.append({"name":spacename,"apistate":"0.13","url":url})
-                if r["state"]["open"] != None:
-                    p = {
-                        "measurement": spacename,
-                        "fields": {
-                            "doorstate": r["state"]["open"]
-                        }
+								if r["state"]["open"] == True:
+									door = 1
+								else
+									door = 0
+                p = {
+                    "measurement": spacename,
+                    "fields": {
+                        "doorstate": door
                     }
-                    print(p)
-                    points.append(p)
+                }
+                print(p)
+                points.append(p)
             except Exception as e:
                 logger.info(spacename + ":" + str(e))
                 directory.append({"name":spacename,"apistate":"invalid","url":url})
